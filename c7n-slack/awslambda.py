08ebb762a17e6630470c523276dc72e40c014db6
Jim Razmus II <jim.razmus@code42.com>
2017-12-04 15:26:40 -0600

    One less get, but add quotes and a comma.

--- c7n-slack/awslambda.py
+++ c7n-slack/awslambda.py
@@ -47,7 +47,7 @@ def lambda_handler(event, context):
                 'channel': os.environ['slack_channel'],
                 'attachments': [
                     {
-                        "title": m.get('action').get('subject', "Policy Violation Detected")
+                        "title": m['action'].get('subject', "Policy Violation Detected"),
                         "fields": [
                             {
                                 "title": "Policy",
@@ -60,7 +60,7 @@ def lambda_handler(event, context):
                                 "short": True
                             }
                         ],
-                        "color": m.get('action').get('cc', "#F35A00")
+                        "color": """ + m['action'].get('cc', "#F35A00") + """
                     },
                     {
                         'title': "Violation",
